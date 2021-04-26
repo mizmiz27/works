80479f8b78515a28fed19bd48ad9731fd4a35497
水本眞史 <mizumoto.web@gmail.com>
2021-04-27 04:35:47 +0900

    トップページの初回アップロード

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,18 @@ var mqpacker = require('css-mqpacker');
 var merge = require('merge-stream');
 
 gulp.task('sass', function() {
+  var topPage = gulp.src('./sass/**/*.scss')
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss([mqpacker()]))
+    .pipe(postcss([cssdeclsort({order: 'smacss'})]))
+    .pipe(postcss([assets({loadPaths: ['images/']})]))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
+
   var musashinoDental = gulp.src('./musashino_dental/sass/**/*.scss')
   //return gulp.src('./musashino_dental/sass/**/*.scss')
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
@@ -25,46 +37,47 @@ gulp.task('sass', function() {
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./musashino_dental/css'));
 
-    var oheroPapa = gulp.src('./ohero_papa/sass/**/*.scss')
-      .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-      .pipe(sourcemaps.init())
-      .pipe(sassGlob())
-      .pipe(sass({outputStyle: 'expanded'}))
-      .pipe(postcss([mqpacker()]))
-      .pipe(postcss([cssdeclsort({order: 'smacss'})]))
-      .pipe(postcss([assets({loadPaths: ['images/']})]))
-      .pipe(postcss([autoprefixer()]))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('./ohero_papa/css'));
+  var oheroPapa = gulp.src('./ohero_papa/sass/**/*.scss')
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss([mqpacker()]))
+    .pipe(postcss([cssdeclsort({order: 'smacss'})]))
+    .pipe(postcss([assets({loadPaths: ['images/']})]))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./ohero_papa/css'));
 
-      var smbcc = gulp.src('./smbcc/sass/**/*.scss')
-      .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-      .pipe(sourcemaps.init())
-      .pipe(sassGlob())
-      .pipe(sass({outputStyle: 'expanded'}))
-      .pipe(postcss([mqpacker()]))
-      .pipe(postcss([cssdeclsort({order: 'smacss'})]))
-      .pipe(postcss([assets({loadPaths: ['images/']})]))
-      .pipe(postcss([autoprefixer()]))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('./smbcc/css'));
+  var smbcc = gulp.src('./smbcc/sass/**/*.scss')
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss([mqpacker()]))
+    .pipe(postcss([cssdeclsort({order: 'smacss'})]))
+    .pipe(postcss([assets({loadPaths: ['images/']})]))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./smbcc/css'));
 
-      var sumidaMarathon = gulp.src('./sumida_marathon/sass/**/*.scss')
-      .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-      .pipe(sourcemaps.init())
-      .pipe(sassGlob())
-      .pipe(sass({outputStyle: 'expanded'}))
-      .pipe(postcss([mqpacker()]))
-      .pipe(postcss([cssdeclsort({order: 'smacss'})]))
-      .pipe(postcss([assets({loadPaths: ['images/']})]))
-      .pipe(postcss([autoprefixer()]))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('./sumida_marathon/css'));
+  var sumidaMarathon = gulp.src('./sumida_marathon/sass/**/*.scss')
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss([mqpacker()]))
+    .pipe(postcss([cssdeclsort({order: 'smacss'})]))
+    .pipe(postcss([assets({loadPaths: ['images/']})]))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./sumida_marathon/css'));
 
-      return merge(musashinoDental, oheroPapa, smbcc, sumidaMarathon);
+      return merge(topPage, musashinoDental, oheroPapa, smbcc, sumidaMarathon);
 });
 
 gulp.task('sass:watch', function() {
+  gulp.watch('./sass/**/*.scss', gulp.task('sass'));
   gulp.watch('./musashino_dental/sass/**/*.scss', gulp.task('sass'));
   gulp.watch('./ohero_papa/sass/**/*.scss', gulp.task('sass'));
   gulp.watch('./smbcc/sass/**/*.scss', gulp.task('sass'));
---
+++ js/common.js
@@ -0,0 +1,80 @@
+window.onload = function() {
+  Particles.init({
+    selector: '.background'
+  });
+
+  var particles = Particles.init({
+    selector: '.background',
+    sizeVariations: 10,
+    color: ['#00bbdd', '#404B69', '#DBEDF3'],
+    connectParticles: true
+  });
+};
+
+//ページ内スクロールとヘッダー
+$(function(){
+  var windowWidth = $(window).width();
+  var headerHight = 90; //固定ヘッダー分ページ内のリンク先表示を下げる
+  $('a[href^="#"]').on('click', function() {
+  var speed = 500;
+  var href= jQuery(this).attr("href");
+  var target = jQuery(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top-headerHight;
+  $('body,html').animate({scrollTop:position}, speed, 'swing');
+  return false;
+ });
+});
+
+$(window).scroll(function () {
+  var height = $(window).height() - 120;
+  // var animationClass = 'is-animation';
+  if($(window).scrollTop() > height) {
+    $('#header').addClass('scrolled');
+  } else {
+    $('#header').removeClass('scrolled');
+  }
+});
+
+
+//menu_listの最終行を左揃えにするため空の<li>を挿入
+$(function() {
+  var $grid = $('#design ul'),
+    emptyCells = [],
+    i;
+// 子パネル (li.cell) の数だけ空の子パネル (li.cell.is-empty) を追加する。
+for (i = 0; i < $grid.find('li').length; i++) {
+    emptyCells.push($('<li>', { class: 'cell is-empty' }));
+}
+$grid.append(emptyCells);
+});
+
+
+//モーダル
+
+$(function(){
+
+  $('.modal').append('<a class="modalClose" href=""><i class="fas fa-times"></i></a>');
+  
+  var posi;
+ 
+  // モーダルウィンドウが開くときの処理
+  $(".modalOpen").click(function(){
+    $('.modal').prepend('<div class="overlay"></div>');
+    //スクロール位置を取得
+    posi = $(window).scrollTop();
+    var href = $(this).attr("href");
+    $(href).fadeIn();
+    $('body').addClass('modal-open').css({'top': -posi});
+    return false;
+  });
+   
+  // モーダルウィンドウが閉じるときの処理
+  $('body').on('click', '.modalClose, .overlay', function(){
+    $(this).parents(".modal").fadeOut();
+    $('.overlay').remove();
+    $('body').removeClass('modal-open').css({'top': 0});
+    $(window).scrollTop(posi);
+    return false;
+  });  
+
+});
